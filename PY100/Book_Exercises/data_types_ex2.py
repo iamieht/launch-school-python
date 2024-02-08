3c8c2d0dcf05762b5f071713eb89bb3025bd75c5
Iván E. Hernández T <iamieht@gmail.com>
2024-02-08 14:10:28 +0100

    Create data_types_ex2.py
    Data Types - Ex2

---
+++ PY100/Book_Exercises/data_types_ex2.py
@@ -0,0 +1,10 @@
+# Create a tuple called names that contains several pet names. For instance:
+
+names = (
+    'Asta', 
+    'Buterscotch', 
+    'Pudding', 
+    'Neptune', 
+    'Darwin',
+)
+print(type(names))

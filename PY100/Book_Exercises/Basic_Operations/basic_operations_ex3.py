4f878bd9d3e7ce8a2cb8a75f5a65eea4a5e49996
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-02-09 13:19:43 +0100

    Basic Operations - Ex3

---
+++ PY100/Book_Exercises/Basic_Operations/basic_operations_ex3.py
@@ -0,0 +1,4 @@
+# What does the following code do? Why?
+print('5' + '10')
+
+# Strings '5' and '10' are concatenated resulting in the string '510'. The '+' operator concatenates strings together and returns a new string.

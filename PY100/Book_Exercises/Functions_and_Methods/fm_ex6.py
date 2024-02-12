3b6b2d6a10c5ed4ac9eb3eb94246dececfcf088f
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-02-12 20:09:57 +0100

    Functions and Methods - Ex6

---
+++ PY100/Book_Exercises/Functions_and_Methods/fm_ex6.py
@@ -0,0 +1,11 @@
+# What does the following code print?
+
+def scream(words):
+    words = words + '!!!!'
+    return
+    print(words)
+
+scream('Yipeee')
+
+# Nothing will be printed and the value returned is None
+

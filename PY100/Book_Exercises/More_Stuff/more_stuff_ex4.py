1a0743d69f989d505588e89794d5468f3d91e815
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-02-18 19:47:26 +0100

    More Stuff - Ex4

---
+++ PY100/Book_Exercises/More_Stuff/more_stuff_ex4.py
@@ -0,0 +1,21 @@
+# Write a function called increment_counter that increments a counter variable every time it is called.
+counter = 0
+g
+
+
+def increment_counter():
+    global counter
+    counter += 1
+
+
+print(counter)  # 0
+
+increment_counter()
+print(counter)  # 1
+
+increment_counter()
+print(counter)  # 2
+
+counter = 100
+increment_counter()
+print(counter)  # 101

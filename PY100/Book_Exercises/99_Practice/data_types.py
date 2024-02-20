79888e8fef576d0797113b79f7f094900033b56f
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-02-20 17:21:38 +0100

    Practice: Data Types - Ex1

--- PY100/Book_Exercises/99_Practice/data_types.py
+++ PY100/Book_Exercises/99_Practice/data_types.py
@@ -1,12 +1,18 @@
-# 1. Identify the data types
+# 1. Identify the data types and class
 
-print(type('True').__name__)            # => str
-print(type(False).__name__)             # => bool
-print(type((1, 2, 3)).__name__)         # => tuple
-print(type(1.5).__name__)               # => float
-print(type([1, 2, 3]).__name__)         # => list
-print(type(2).__name__)                 # => int
-print(type(range(5)).__name__)          # => range
-print(type({1, 2, 3}).__name__)         # => set
-print(type(None).__name__)              # => NoneType
-print(type({'foo': 'bar'}).__name__)    # => dict
+print(type('True').__name__)            # => String, `str`
+print(type(False).__name__)             # => Boolean, `bool
+print(type((1, 2, 3)).__name__)         # => Tuple, `tuple`
+print(type(1.5).__name__)               # => Float, `float`
+print(type([1, 2, 3]).__name__)         # => List, `list`
+print(type(2).__name__)                 # => Integer, `int``
+print(type(range(5)).__name__)          # => Range, `range``
+print(type({1, 2, 3}).__name__)         # => Set,   `set``
+print(type(None).__name__)              # => NoneType, `NoneType``
+print(type({'foo': 'bar'}).__name__)    # => Dicionary, `dict``
+
+# The data type is the universal type label, the class is the actual type (class) in Python that implements the data type
+
+# For ranges, Python doesn't have a literal syntax to create them, so we need to use a constructor function range() to build one
+
+# None represents the absence of a value

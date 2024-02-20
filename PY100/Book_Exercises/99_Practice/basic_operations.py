5e3358d73c39c76f18775e0c0bf629112fbda239
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-02-20 17:50:49 +0100

    Practice: Basic Operations - Ex1

---
+++ PY100/Book_Exercises/99_Practice/basic_operations.py
@@ -0,0 +1,12 @@
+# 1. Concatenate two strings, one with your first name and one with your last, to create a new string with your full name as its value.
+
+print('-------------String Concatenation-------------------')
+my_name = 'Ivan' + ' ' + 'Hernandez'
+print(my_name)
+
+first_name = 'Ivan'
+last_name = 'Hernandez'
+print(first_name + ' ' + last_name)
+
+print('-------------String Interpolation-------------------')
+print(f'{first_name} {last_name}')

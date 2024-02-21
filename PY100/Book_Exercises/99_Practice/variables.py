bb9ed649e4c6635d64d6ebfb78fb654da5f1b614
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-02-21 18:23:26 +0100

    Practice: Variables Ex2

--- PY100/Book_Exercises/99_Practice/variables.py
+++ PY100/Book_Exercises/99_Practice/variables.py
@@ -10,3 +10,16 @@
 # operand2       idiomatic
 # BIG_NUMBER     non-idiomatic   SCREAMING_SNAKE_CASE reserved for constants
 # π              non-idiomatic   non ASCII character set
+
+# 2. Classify the following potential function names as idiomatic, non-idiomatic, or illegal. For the non-idiomatic and illegal names, explain your choice.
+
+# Name      Classification
+# -------------------------
+# index          idiomatic
+# CatName        non-idiomatic   CamelCase is used which is reserved for Classes
+# lazy_dog       idiomatic
+# quick_Fox      non-idiomatic   all letters must be in lowercase
+# 1stCharacter   illegal         must start with a character
+# operand2       idiomatic
+# BIG_NUMBER     non-idiomatic   SCREAMING_SNAKE_CASE reserved for constants
+# π              non-idiomatic   non ASCII character set

b19e517ebd1eecddb76213730250cc648d80dcf5
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-02-21 18:19:48 +0100

    Practice: Variables Ex1

---
+++ PY100/Book_Exercises/99_Practice/variables.py
@@ -0,0 +1,12 @@
+# 1. Classify the following potential non-constant variable names as idiomatic, non-idiomatic, or illegal. For the non-idiomatic and illegal names, explain your choice.
+
+# Name      Classification
+# -------------------------
+# index          idiomatic
+# CatName        non-idiomatic   CamelCase is used which is reserved for Classes
+# lazy_dog       idiomatic
+# quick_Fox      non-idiomatic   all letters must be in lowercase
+# 1stCharacter   illegal         must start with a character
+# operand2       idiomatic
+# BIG_NUMBER     non-idiomatic   SCREAMING_SNAKE_CASE reserved for constants
+# π              non-idiomatic   non ASCII character set

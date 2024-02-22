59d7ebe356f5fd50854c52cf83552ec9cca7a114
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-02-22 20:44:50 +0100

    Practice Function Methods

--- PY100/Book_Exercises/99_Practice/functions_methods.py
+++ PY100/Book_Exercises/99_Practice/functions_methods.py
@@ -24,7 +24,7 @@ print(foo)
 print()
 print("-" * 60)
 
-# Write a program that uses a multiply function to multiply two numbers and returns the result. Ask the user to enter the two numbers, then output the numbers and result as a simple equation.
+# 3. Write a program that uses a multiply function to multiply two numbers and returns the result. Ask the user to enter the two numbers, then output the numbers and result as a simple equation.
 
 
 def multiply(number1, number2):

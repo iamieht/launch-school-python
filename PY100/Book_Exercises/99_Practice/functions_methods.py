2559a94fac5877c06e944da6fcb4ce6851496882
Iván E. Hernández T <iamieht@gmail.com>
2024-02-23 15:02:05 +0100

    Update functions_methods.py
    Ex16

--- PY100/Book_Exercises/99_Practice/functions_methods.py
+++ PY100/Book_Exercises/99_Practice/functions_methods.py
@@ -219,3 +219,22 @@ print(f'{first_number} * {second_number} = {product}')
 # Global:       # multiply, get_num, first_number, second_number, product
 # Local:        # left, right, prompt
 # Built-in      # float, input, print
+
+# 16. In the code shown below, identify all of the function names and parameters present in the code
+
+
+def multiply(left, right):
+    return left * right
+
+
+def get_num(prompt):
+    return float(input(prompt))
+
+
+first_number = get_num('Enter the first number: ')
+second_number = get_num('Enter the second number: ')
+product = multiply(first_number, second_number)
+print(f'{first_number} * {second_number} = {product}')
+
+# function names:       # multiply, get_num, float, input, print
+# parameters:           # left, right, prompt

b2caaa7890f4db4b04a0252089645e8406c689f5
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-02-24 15:21:32 +0100

    Intro to Collections - Ex7

--- PY100/Book_Exercises/99_Practice/intro_collections.py
+++ PY100/Book_Exercises/99_Practice/intro_collections.py
@@ -27,3 +27,10 @@ print(stuff)
 pi = 3.141592
 str_pi = str(pi)
 print(str_pi)
+
+# 7. Without running the following code, identify the numbers that are included in each of the following ranges:
+print(list(range(1, 6)))         # 1, 2, 3, 4, 5
+print(list(range(7)))            # 0, 1, 2, 3, 4, 5, 6
+print(list(range(3, 15, 4)))     # 3, 7, 11
+print(list(range(3, 8, -1)))     # []
+print(list(range(8, 3, -1)))     # 8, 7, 6, 5, 4

57e334bbbc937b315d3d1d215e1aa4ecc7b0efef
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-02-25 09:52:58 +0100

    Practice: Using Collections Ex15

--- PY100/Book_Exercises/99_Practice/using_collections.py
+++ PY100/Book_Exercises/99_Practice/using_collections.py
@@ -181,3 +181,16 @@ my_range = range(10, 60, 10)
 
 result = zip(my_str, my_list, my_tuple, my_range)
 print(list(result))
+
+# 15. Without running the following code, what values will be printed by line 10?
+print('-' * 60)
+pets = {
+    'Cat':  'Meow',
+    'Dog':  'Bark',
+    'Bird': 'Tweet',
+}
+
+keys = pets.keys()
+del pets['Dog']
+pets['Snake'] = 'Sssss'
+print(keys)     # ['Cat', 'Bird', 'Snake']

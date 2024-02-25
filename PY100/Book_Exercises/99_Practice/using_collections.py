be04b61f1dd199548681fec7e67e7f2c77c92251
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-02-25 09:50:09 +0100

    Practice: Using Collections Ex14

--- PY100/Book_Exercises/99_Practice/using_collections.py
+++ PY100/Book_Exercises/99_Practice/using_collections.py
@@ -171,3 +171,13 @@ print('Butterscotch' in cats)   # True
 print('Butter' in cats)         # False
 print('Butter' in cats[3])      # True
 print('cheddar' in cats)        # False
+
+# 14. Write some code that combines the sequences into a list of tuples. Each tuple should contain one member of each sequence. Print the final result so you can see all the values
+print('-' * 60)
+my_str = 'abc'
+my_list = ['Alpha', 'Bravo', 'Charlie']
+my_tuple = (None, True, False)
+my_range = range(10, 60, 10)
+
+result = zip(my_str, my_list, my_tuple, my_range)
+print(list(result))

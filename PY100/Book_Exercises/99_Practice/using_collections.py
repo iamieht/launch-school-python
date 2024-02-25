1c65f4d0cbc3ba8d56e1ce64c427551fc521565f
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-02-25 09:20:45 +0100

    Practice: Using Collections Ex6

--- PY100/Book_Exercises/99_Practice/using_collections.py
+++ PY100/Book_Exercises/99_Practice/using_collections.py
@@ -60,3 +60,18 @@ range(5)
 3
 0.0
 frozenset({1, 4, 9, 16, 25})
+
+# 6. What will the following code print?
+print('abc-def'.isalpha())          # False
+print('abc_def'.isalpha())          # False
+print('abc def'.isalpha())          # False
+print('abc def'.isalpha() and       # False
+      'abc def'.isspace())
+print('abc def'.isalpha() or        # False
+      'abc def'.isspace())
+print('abcdef'.isalpha())           # True
+print('31415'.isdigit())            # True
+print('-31415'.isdigit())           # False
+print('31_415'.isdigit())           # False
+print('3.1415'.isdigit())           # False
+print(''.isspace())                 # False

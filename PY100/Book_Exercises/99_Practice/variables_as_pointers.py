a3d7bbdcc572c91f45e910ec89812b4ac3741ef5
Iván E. Hernández T <iamieht@gmail.com>
2024-02-26 15:22:10 +0100

    Update variables_as_pointers.py
    Ex6

--- PY100/Book_Exercises/99_Practice/variables_as_pointers.py
+++ PY100/Book_Exercises/99_Practice/variables_as_pointers.py
@@ -56,3 +56,21 @@ print(dict1['a'][1] is dict2['a'][1])
 print(dict1['b'] is dict2['b'])
 print(dict1['b'][0] is dict2['b'][0])
 print(dict1['b'][1] is dict2['b'][1])
+
+# 6. The following program is nearly identical to that of the previous exercise. However, this time, it should create a shallow copy of dict1.
+print('-' * 60)
+dict1 = {
+    'a': [{7, 1}, ['aa', 'aaa'], 2],
+    'b': ({3, 2}, ['bb', 'bbb']),
+}
+
+dict2 = dict(dict1)
+
+print(dict1 is dict2)
+print(dict1['a'] is dict2['a'])
+print(dict1['a'][0] is dict2['a'][0])
+print(dict1['a'][1] is dict2['a'][1])
+print(dict1['a'][2] is dict2['a'][2])
+print(dict1['b'] is dict2['b'])
+print(dict1['b'][0] is dict2['b'][0])
+print(dict1['b'][1] is dict2['b'][1])

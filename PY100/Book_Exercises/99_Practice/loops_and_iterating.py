d55c05f97697e4caff0eb0bc95c09c2665a1b0c0
Iván E. Hernández T <iamieht@gmail.com>
2024-02-26 09:30:29 +0100

    Update loops_and_iterating.py
    Ex4

--- PY100/Book_Exercises/99_Practice/loops_and_iterating.py
+++ PY100/Book_Exercises/99_Practice/loops_and_iterating.py
@@ -26,3 +26,18 @@ while index < len(my_list):
 print()
 for element in my_list:
     print(element)
+
+# 4. Use a while loop to print all numbers in my_list with even values, one number per line. Then, print the odd numbers using a ' for' loop.
+print('-' * 60)
+my_list = [6, 3, 0, 11, 20, 4, 17]
+index = 0
+
+while index < len(my_list):
+    if my_list[index] % 2 == 0:
+        print(my_list[index])
+    index += 1
+
+print()
+for element in my_list:
+    if element % 2 != 0:
+        print(element)

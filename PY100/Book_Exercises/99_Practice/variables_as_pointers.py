2556db0ee844cc79ac8c7771f9d6c4d52a1f2af8
Iván E. Hernández T <iamieht@gmail.com>
2024-02-26 14:51:50 +0100

    Update variables_as_pointers.py
    Ex3

--- PY100/Book_Exercises/99_Practice/variables_as_pointers.py
+++ PY100/Book_Exercises/99_Practice/variables_as_pointers.py
@@ -11,4 +11,17 @@ set2 = set1
 set1.add(range(5, 10))
 print(set2)  # => {42, 'Monty Python', ('a', 'b', 'c'), range(5, 10)}
 
-# set1 and set2 reference the same object in memory, as in line 10 set2 was assigned a shallow copy of set1
+# set1 and set2 reference the same object in memory.
+
+# 3. Without running this code, what will it print? Why?
+dict1 = {
+    "Hitchhiker's Guide to the Galaxy": 42,
+    'Monty Python': 'The Life of Brian',
+    'Airplane!': "Don't call me Shirley!",
+}
+
+dict2 = dict(dict1)
+dict2['Monty Python'] = 'Holy Grail'
+print(dict1['Monty Python'])  # => The Life of Brian
+
+# dict2 is a new object (a shallow copy of dict1) in memory, so a modification of dict2 doesn't affect the referenced object of dict1

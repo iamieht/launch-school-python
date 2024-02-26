9031bffe22a6537f575a54463e9165ab9b5cfd50
Iván E. Hernández T <iamieht@gmail.com>
2024-02-26 14:46:56 +0100

    Create variables_as_pointers.py
    Variables as Pointers Ex1

---
+++ PY100/Book_Exercises/99_Practice/variables_as_pointers.py
@@ -0,0 +1,6 @@
+# 1. In your own words, explain the difference between these two expressions.
+my_object1 == my_object2
+my_object1 is my_object2
+
+# The first expression compares whether the two objects have the same value
+# The second expression compares whether both objects reference the same object.

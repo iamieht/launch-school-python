3b202ce6700b55136f2535d060fa4f0d7fac313e
Iván E. Hernández T <iamieht@gmail.com>
2024-02-26 13:56:04 +0100

    Update loops_and_iterating.py
    Ex6

--- PY100/Book_Exercises/99_Practice/loops_and_iterating.py
+++ PY100/Book_Exercises/99_Practice/loops_and_iterating.py
@@ -54,3 +54,14 @@ for lst in my_list:
     for element in lst:
         if element % 2 == 0:
             print(element)
+
+# 6. In this problem, you should write code that creates a new list with one element for each number in my_list. If the original number is an even, then the corresponding element in the new list should contain the string 'even'; otherwise, the element should contain 'odd'.
+print('-' * 60)
+my_list = [
+    1, 3, 6, 11,
+    4, 2, 4, 9,
+    17, 16, 0,
+]
+
+new_list = ['even' if element % 2 == 0 else 'odd' for element in my_list]
+print(new_list)

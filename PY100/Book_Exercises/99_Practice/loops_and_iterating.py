ece166e786a785837865d2956f627bb35bf94a78
Iván E. Hernández T <iamieht@gmail.com>
2024-02-26 14:09:35 +0100

    Update loops_and_iterating.py
    Ex9

--- PY100/Book_Exercises/99_Practice/loops_and_iterating.py
+++ PY100/Book_Exercises/99_Practice/loops_and_iterating.py
@@ -92,3 +92,25 @@ my_set = {
 my_dict = {element: len(element)
            for element in my_set if len(element) % 2 != 0}
 print(my_dict)
+
+# 9. Write a function that computes and returns the factorial of a number by using a for or while loop. The factorial of a positive integer n, signified by n!, is defined as the product of all integers between 1 and n:
+print('-' * 60)
+
+
+def factorial(number):
+    result = 1
+    for num in range(1, number + 1):
+        result *= num
+
+    return result
+
+
+print(factorial(1))   # 1
+print(factorial(2))   # 2
+print(factorial(3))   # 6
+print(factorial(4))   # 24
+print(factorial(5))   # 120
+print(factorial(6))   # 720
+print(factorial(7))   # 5040
+print(factorial(8))   # 40320
+print(factorial(25))  # 15511210043330985984000000

17ab1fb90b4e333f47aadab9fb37c72be8f10c44
Iván E. Hernández T <iamieht@gmail.com>
2024-02-27 10:43:59 +0100

    Create lowercase.py
    Lowercase

---
+++ PY100/Python_Basics/Reading_Documentation_1/lowercase.py
@@ -0,0 +1,6 @@
+# Determine whether Python has a method to lowercase a string, for example converting 'Aloha, World!' into 'aloha, world!.
+
+lowercase = 'Aloha, World!'.lower()
+print(lowercase)
+
+# str.lower() => returns a copy of the string with all the cases characters converted to lowercase.

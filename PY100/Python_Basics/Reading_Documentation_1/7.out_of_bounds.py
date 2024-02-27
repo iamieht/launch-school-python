928d4125a77f623f479ce4d6b0ab833efa940ffe
Iván E. Hernández T <iamieht@gmail.com>
2024-02-27 12:42:57 +0100

    Create 7.out_of_bounds.py
    Out of Bounds

---
+++ PY100/Python_Basics/Reading_Documentation_1/7.out_of_bounds.py
@@ -0,0 +1,5 @@
+# What happens if we take the list ['fish', 'and', 'chips'] and try to access the element at index position 10?
+lst = ['fish', 'and', 'chips']
+idx10 = lst[10]
+
+# This will raise an exception IndexError: list index out of rabnge, as indexing access works only with inbound indexes.

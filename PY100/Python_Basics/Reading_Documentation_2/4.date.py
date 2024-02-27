0b90e3768f36569f680d7ef7197f481c18fe38cb
Iván E. Hernández T <iamieht@gmail.com>
2024-02-27 14:26:55 +0100

    Create 4.date.py
    Date

---
+++ PY100/Python_Basics/Reading_Documentation_2/4.date.py
@@ -0,0 +1,6 @@
+# Using the datetime module in Python, how would you obtain the current date and time?
+from datetime import datetime
+date = datetime.today()
+now = datetime.now()
+print(date)
+print(now)

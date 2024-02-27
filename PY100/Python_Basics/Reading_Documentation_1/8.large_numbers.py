e8f595b4b3b5a9053d980b08099ff9c1001768a1
Iván E. Hernández T <iamieht@gmail.com>
2024-02-27 12:51:36 +0100

    Create 8.large_numbers.py
    Large Numbers

---
+++ PY100/Python_Basics/Reading_Documentation_1/8.large_numbers.py
@@ -0,0 +1,5 @@
+# determine how you can write large numbers in a way that makes them easier to read.
+
+print(100_000_000_000_000)
+print(1_00_000_000_000_000)
+# Underscores are ignored for determining the numeric value of the literal.

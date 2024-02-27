0be9926025f8658bd3bf60ac8edd7105aa2b1b95
Iván E. Hernández T <iamieht@gmail.com>
2024-02-27 14:44:30 +0100

    Create 9.type_error.py
    type error

---
+++ PY100/Python_Basics/Reading_Documentation_2/9.type_error.py
@@ -0,0 +1,11 @@
+tweet = 'Woohoo! :-)'
+
+if len(tweet) > 140:
+    print('Tweet is too long!')
+
+length_of_tweet = len(tweet) + 5
+print(length_of_tweet)
+
+
+# # exception TypeError
+# Raised when an operation or function is applied to an object of inappropriate type. The associated value is a string giving details about the type mismatch.

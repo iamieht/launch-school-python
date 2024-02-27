621fff8eac99b89a211821a97d542598a3a87ed9
Iván E. Hernández T <iamieht@gmail.com>
2024-02-27 08:41:48 +0100

    Update more_stuff.py
    Ex3

--- PY100/Book_Exercises/99_Practice/more_stuff.py
+++ PY100/Book_Exercises/99_Practice/more_stuff.py
@@ -25,3 +25,17 @@ print(do_something(my_dict))
 print(math.sqrt(37))
 print(sqrt(37))
 print(m.sqrt(37))
+
+# 3. Write a nested function in sum_of_squares that will make this code work as shown.
+
+
+def square(num):
+    return num * num
+
+
+def sum_of_squares(num1, num2):
+    return square(num1) + square(num2)
+
+
+print(sum_of_squares(3, 4))   # 25 (3 * 3 + 4 * 4)
+print(sum_of_squares(5, 12))  # 169 (5 * 5 + 12 * 12)

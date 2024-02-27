e4f5fcd03f6a8d5490695784f582b2f4d31fcb98
Iván E. Hernández T <iamieht@gmail.com>
2024-02-27 10:58:31 +0100

    Create right_justifying_strings.py
    right justifying strings

---
+++ PY100/Python_Basics/Reading_Documentation_1/right_justifying_strings.py
@@ -0,0 +1,7 @@
+# Use the Python documentation for the str class to determine which method can be used to right justify a str object.
+# str = "hello"
+# print(str)
+# right_justified = str.rjust(10, '*')
+# print(right_justified)
+for i in range(1, 10):
+    print("*".rjust(i, '*'))

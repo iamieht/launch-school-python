abc7ebda6e9819e2a6e46609634f0d85e2d69f14
Iván E. Hernández T <iamieht@gmail.com>
2024-02-27 08:34:34 +0100

    Create more_stuff.py
    More Stuff - Ex1

---
+++ PY100/Book_Exercises/99_Practice/more_stuff.py
@@ -0,0 +1,17 @@
+# 1. What does the following function do? Be sure to identify the output value.
+def do_something(dictionary):
+    return sorted(dictionary.keys())[1].upper()
+
+
+my_dict = {
+    'Karl':     108,
+    'Clare':    175,
+    'Karis':    140,
+    'Trevor':   180,
+    'Antonina': 132,
+    'Chris':    101,
+}
+
+print(do_something(my_dict))
+# Step 1: ['Antonina', 'Chris', 'Clare', 'Karis', 'Karl']
+# Step 2: CHRIS

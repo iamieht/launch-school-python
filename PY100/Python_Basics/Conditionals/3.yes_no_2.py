c56bbe9ad2bbf2659b4fa9e8a4cbbace8fccf59f
Iván E. Hernández T <iamieht@gmail.com>
2024-02-28 08:06:56 +0100

    Create 3.yes_no_2.py
    Ex3

---
+++ PY100/Python_Basics/Conditionals/3.yes_no_2.py
@@ -0,0 +1,7 @@
+# Rewrite your code from the previous exercise to use a ternary expression.
+import random
+random_number = random.randint(0, 1)
+
+print(f'Random number is: {random_number}')
+
+print('Yes!' if random_number else 'No.')

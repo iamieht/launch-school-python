d4128979189de86b9ebafd14a45b95a6b44c5ebd
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-02-28 20:18:29 +0100

    Functions - Ex3

---
+++ PY100/Python_Basics/Functions/3.cite_the_author.py
@@ -0,0 +1,6 @@
+# Implement a function named cite that takes two string arguments: the author of the quote and what they said. It then prints the quote
+def cite(author, quote):
+    print(f'{author} said: "{quote}"')
+
+
+cite('Bruce Eckel', 'Python is executable pseudocode.')

b063bc750a2b9fde4dc95378a4b65f1434863099
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-02-28 20:20:13 +0100

    Functions - Ex4

---
+++ PY100/Python_Basics/Functions/4.squared_number.py
@@ -0,0 +1,6 @@
+# Write a function that accepts a single argument, a number, and returns the result of multiplying its argument by an exponent of 2
+def squared_number(number):
+    return number ** 2
+
+
+print(squared_number(3))   # 9

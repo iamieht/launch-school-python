30a9e40b443e9087a30bd4f2e6ea872446d0a63b
Iván E. Hernández T <iamieht@gmail.com>
2024-02-29 08:14:16 +0100

    Create 7.transformation.py
    Ex7

---
+++ PY100/Python_Basics/Functions/7.transformation.py
@@ -0,0 +1,4 @@
+# Use Python's string methods on the string 'Captain Ruby' to create a string with the value 'Captain Python'.
+string = 'Captain Ruby'
+string2 = string.replace('Ruby', 'Python')
+print(string2)

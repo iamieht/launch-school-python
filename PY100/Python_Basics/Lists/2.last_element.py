cd2a167670f01a1f2893f5d993e41af2298a5aef
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-02-29 18:24:01 +0100

    Lists - Ex2

---
+++ PY100/Python_Basics/Lists/2.last_element.py
@@ -0,0 +1,9 @@
+# Write a function that returns the last element of a list provided as an argument.
+def last(lst):
+    if lst:
+        return lst[-1]
+    else:
+        return None
+
+
+print(last(['Earth', 'Moon', 'Mars']))  # Mars

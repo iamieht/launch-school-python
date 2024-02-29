53778ff576378e2b74037936637bcffa5173a45a
Iván E. Hernández T <iamieht@gmail.com>
2024-02-29 14:52:33 +0100

    Create 8.whats_my_value_8.py
    Ex8

---
+++ PY100/Python_Basics/Variable_Scope/8.whats_my_value_8.py
@@ -0,0 +1,7 @@
+# What will the following code do and why?
+print(greeting)
+
+greeting = 'Hello world!'
+
+# It raises an exception: NameError as we are trying to use a variable before it is initialized.
+# In Python, you must initialize a variable before you use it; otherwise, it raises a NameError indicating the name hasn't been defined.

1f6a494e2e3b31d8ffa82e29529feb37b911a582
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-02-29 17:55:28 +0100

    Strings - Ex5

---
+++ PY100/Python_Basics/Strings/5.contains_character.py
@@ -0,0 +1,6 @@
+# Write code that checks whether the string char_sequence contains the character x.
+char_sequence = 'TXkgaG92ZXJjcmFmdCBpcyBmdWxsIG9mIGVlbHMu'
+
+print('x' in char_sequence)
+
+# You can check whether a substring exists within a string using the in keyword.

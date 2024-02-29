07649d86e01836cb733d290c32206358500b685e
Iván E. Hernández T <iamieht@gmail.com>
2024-02-29 15:05:05 +0100

    Create 2.all_caps.py
    Ex2

---
+++ PY100/Python_Basics/Strings/2.all_caps.py
@@ -0,0 +1,5 @@
+# Convert the string 'confetti floating everywhere' to all upper case.
+string = 'confetti floating everywhere'
+print(string.upper())
+
+# We use the upper method of a string to transform all its characters to uppercase. This method returns a new string with all characters in uppercase. The original string remains unchanged.

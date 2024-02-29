c709f334ab64fb97cc5380216deb015a74d92584
Iván E. Hernández T <iamieht@gmail.com>
2024-02-29 15:03:49 +0100

    Create 1.length.py
    Strings - Ex1

---
+++ PY100/Python_Basics/Strings/1.length.py
@@ -0,0 +1,7 @@
+# Determine the length of the string "These aren't the droids you're looking for.".
+string = "These aren't the droids you're looking for."
+print(len(string))
+
+# We can determine the length of a string using the built-in len function. An empty string will have a length of 0.
+
+# The len function also works with all built-in collections, such as lists, sets, ranges, and dictionaries.

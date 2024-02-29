671af8a9b79e0d63e8ecbe74e159fcbe519a3085
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-02-29 19:58:08 +0100

    Dictionaries - Ex2

---
+++ PY100/Python_Basics/Dictionaries/2.adding_the_year.py
@@ -0,0 +1,9 @@
+# Add some code below the following code to define a year key with a value of 2003.
+car = {
+    'type':    'sedan',
+    'color':   'blue',
+    'mileage': 80_000,
+}
+
+car['year'] = 2003
+print(car)

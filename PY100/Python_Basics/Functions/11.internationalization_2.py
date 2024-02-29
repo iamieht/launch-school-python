e7bf552d616e455d3f4774891af5aeaac1b9439c
Iván E. Hernández T <iamieht@gmail.com>
2024-02-29 08:33:44 +0100

    Create 11.internationalization_2.py
    Ex11

---
+++ PY100/Python_Basics/Functions/11.internationalization_2.py
@@ -0,0 +1,48 @@
+# write a function local_greet that takes a locale as input, and returns a greeting.
+def greet(isolanguage):
+    match isolanguage:
+        case 'en':
+            return 'Hi!'
+        case 'fr':
+            return 'Salut!'
+        case 'pt':
+            return 'Olá!'
+        case 'de':
+            return 'Hallo!'
+        case 'sv':
+            return 'Hej!'
+        case 'af':
+            return 'Haai!'
+        case _:
+            return 'Hello!'
+
+
+def extract_language(locale):
+    return locale[:2]
+
+
+def extract_region(locale):
+    return locale[3:5]
+
+
+def local_greet(locale):
+    isolanguage = extract_language(locale)
+    region = extract_region(locale)
+
+    match (isolanguage, region):
+        case ('en', 'US'):
+            return 'Hey!'
+        case ('en', 'GB'):
+            return 'Hello!'
+        case ('en', 'AU'):
+            return 'Howdy!'
+        case _:
+            return greet(isolanguage)
+
+
+print(local_greet('en_US.UTF-8'))       # Hey!
+print(local_greet('en_GB.UTF-8'))       # Hello!
+print(local_greet('en_AU.UTF-8'))       # Howdy!
+print(local_greet('fr_FR.UTF-8'))       # Salut!
+print(local_greet('fr_CA.UTF-8'))       # Salut!
+print(local_greet('fr_MA.UTF-8'))       # Salut!

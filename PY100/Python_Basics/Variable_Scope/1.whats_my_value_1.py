2f198caa9771bb876e3958b196aba21d26e5c8c1
Iván E. Hernández T <iamieht@gmail.com>
2024-02-29 14:43:23 +0100

    Update 1.whats_my_value_1.py
    Ex1

--- PY100/Python_Basics/Variable_Scope/1.whats_my_value_1.py
+++ PY100/Python_Basics/Variable_Scope/1.whats_my_value_1.py
@@ -9,9 +9,9 @@ print(my_value)  # => 20
 # In Python, variables initialized inside a block (like inside an if statement) are accessible outside of that block. Thus, the variable my_value initialized inside the if block on line 2 can be accessed and printed on line 4. The output of the print(my_value) statement is 20.
 
 # What do you think will happen if we run the following code instead:
-if False:
-    my_new_value = 42
+# if False:
+#     my_new_value = 42
 
-print(my_new_value)
+# print(my_new_value)
 
 # The variable my_new_value is in the global scope but never initialized as the if condition always evaluate to a falsy value and the block code is never executed, so a NameError exception is raised.

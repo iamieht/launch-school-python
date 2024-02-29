53989f16d1eaa73681da6b35c980f97baf0941db
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-02-29 18:33:52 +0100

    Lists - Ex6

---
+++ PY100/Python_Basics/Lists/6.vocabulary.py
@@ -0,0 +1,18 @@
+# Write some code that iterates through and prints all the words, one per line.
+vocabulary = [
+    ['happy', 'cheerful', 'merry', 'glad'],
+    ['tired', 'sleepy', 'fatigued', 'drained'],
+    ['excited', 'eager', 'enthused', 'animated'],
+]
+
+for element in vocabulary:
+    for word in element:
+        print(word)
+
+# happy
+# cheerful
+# merry
+# glad
+# tired
+# sleepy
+# etc...

d017b387ad167cbbaee69249de153ac14da17c64
Iván E. Hernández T <iamieht@gmail.com>
2024-03-05 13:52:38 +0100

    Update 2.odd_numbers.py
    Ex2 - Further Exploration

--- PY101/Small_Problems/Easy1/2.odd_numbers.py
+++ PY101/Small_Problems/Easy1/2.odd_numbers.py
@@ -33,6 +33,17 @@
 #   - Iterate from number 1 until the integer provided (inclusive) by using a step of 2, so the iteration only happens over the odd numbers
 #   - print the numbers
 
+# Further Exploration
+# Consider adding a way for the user to specify the starting and ending values of the odd numbers printed.
+
+# Algorithm
+# - Define a function odd_numbers_fe with two parameters (start, stop) of type integer
+#   - Iterate from start until the stop (inclusive).
+#   - Define a function is_odd that takes a single integer
+#       - return if the number is odd (number % 2 == 1)
+#   - With each iteration check if the number is odd by invoking the is_odd function
+#   - If the number is odd print it
+
 # Code
 def odd_numbers(integer):
     for num in range(1, integer + 1):
@@ -50,9 +61,17 @@ def odd_numbers_bonus(integer):
         print(num)
 
 
+def odd_numbers_fe(start, stop):
+    for num in range(start, stop + 1):
+        if is_odd(num):
+            print(num)
+
+
 # Examples/Test Cases (E)
 # => 1, 3, 5, 7, 9.....99 (each number printed in its own line)
 print("---- odd numbers -------")
 odd_numbers(99)
 print("---- odd numbers bonus --------")
 odd_numbers_bonus(99)
+print("---- odd numbers further exploration --------")
+odd_numbers_fe(-20, 20)

1e986550a2b5bfc9f0de416035aa52a7aed948ec
Iván E. Hernández T <iamieht@gmail.com>
2024-03-05 14:06:25 +0100

    Update 3.even_numbers.py
    Ex3 - Bonus question

--- PY101/Small_Problems/Easy1/3.even_numbers.py
+++ PY101/Small_Problems/Easy1/3.even_numbers.py
@@ -2,6 +2,8 @@
 
 # Print all even numbers from 1 to 99, inclusive, with each number on a separate line.
 
+# Bonus Question: Can you solve the problem by iterating over just the even numbers?
+
 # - Input:
 #   - Numbers 1 and 99
 # - Output:
@@ -21,7 +23,15 @@
 #   - Check if each number % 2 == 0
 #   - If truthy print number
 
+# Algorithm Bonus
+#  - Iterate over the range 1 to 99 with step of 2
+#   - print the number
+
 # Code
 for num in range(1, 100):
     if num % 2 == 0:
         print(num)
+
+print("-" * 60)
+for num in range(2, 100, 2):
+    print(num)

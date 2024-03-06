bdba4ff74c0082e02e8c7c6284495ab8d1501c1b
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-03-06 17:41:15 +0100

    Study Session 11032024

---
+++ PY101/Study_Sessions/palindrome.py
@@ -0,0 +1,7 @@
+# Write a palindrome function with two integer numbers as parameters ("num" and "s") and outputs a list that contain an "s" number of elements, all palindromes, higher than the first number  "num". No single digit numbers can be palindromes
+# #Examples/Test Cases
+# print(palindromes(6, 4)) # => [11, 22,33,44]
+# print(palindromes(0, 4)) # => [11, 22,33,44]
+# print(palindromes(20, 0) # => []
+# print(palindromes(75, 1)) # => [77]
+# print(palindromes(100, 2)) # => [101, 111 ]

0509edc57c88a43d16af0b431d186b95be26569f
Iván E. Hernández T <iamieht@gmail.com>
2024-03-07 16:53:48 +0100

    Create 07032024.py

---
+++ PY101/Study_Sessions/07032024.py
@@ -0,0 +1,18 @@
+# Ex1
+numbers1 = [1, 2, 3, 4]
+numbers2 = [1, 2, 3, 4]
+numbers3 = numbers2
+
+print(numbers == numbers)
+print(numbers is numbers)
+
+# Ex2
+# Create an empty set
+# Create a range from 1 to 10 of only even numbers
+# Create a range from 1 to 10 of only odd numbers
+# Create a list of elements from the string = "Hello, World!"
+# Create a new list with the string = "Hello, World!" reversed
+# Modify from the 2nd to the 5th element (inclusive) of the following sequence seq = [1, 2, 3, 4, 5, 6, 7, 8, 9] to 'a', 'b', 'c', 'd'
+
+# Ex3
+# Write a function that takes one integer argument and returns True when the number's absolute value is odd, False otherwise.

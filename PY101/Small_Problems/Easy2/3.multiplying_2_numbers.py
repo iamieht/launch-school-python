fb84ccb34a011c879381f8c83b9c4097760e6162
Iván E. Hernández T <iamieht@gmail.com>
2024-03-11 08:27:47 +0100

    Create 3.multiplying_2_numbers.py
    Easy3 - Multiply

---
+++ PY101/Small_Problems/Easy2/3.multiplying_2_numbers.py
@@ -0,0 +1,31 @@
+# Problem (P)
+# Create a function that takes two arguments, multiplies them together, and returns the result.
+
+# - Input:
+#   - number1: float
+#   - number2: float
+
+# - Output:
+#   - a result of the multiplication of number1 and number2: float
+
+# - Explicit Rules:
+#   - 2 numbers as operands
+
+# - Implicit Rules:
+#   - Number types float
+
+# Examples/Test Cases (E)
+# print(multiply(5, 3) == 15)  # True
+
+# Data Structures
+
+# Algorithm
+# - Declare a function multiply that takes 2 arguments (num1, num2)
+#   - return num1 * num2
+
+# Code
+def multiply(num1, num2):
+    return num1 * num2
+
+
+print(multiply(5, 3) == 15)  # True

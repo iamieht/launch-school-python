26ad7fc04a53636ec192ff9913ecabe28ba5f53a
Iván E. Hernández T <iamieht@gmail.com>
2024-03-11 12:09:59 +0100

    Create 8.odd_lists.py
    Easy2 - Odd Lists

---
+++ PY101/Small_Problems/Easy2/8.odd_lists.py
@@ -0,0 +1,56 @@
+# Problem (P)
+# Write a function that returns a list that contains every other element of a list that is passed in as an argument.
+
+# - Input:
+#   - list of n elements
+
+# - Output:
+#   - list of odd elements of original list
+
+# - Explicit Rules:
+#   - The values in the returned list should be those values that are in the 1st, 3rd, 5th, and so on elements of the argument list.
+
+# - Implicit Rules:
+#   - Empty list returns an empty list
+
+# Examples/Test Cases (E)
+# print(oddities([2, 3, 4, 5, 6]) == [2, 4, 6])  # True
+# print(oddities([1, 2, 3, 4]) == [1, 3])        # True
+# print(oddities(["abc", "def"]) == ['abc'])     # True
+# print(oddities([123]) == [123])                # True
+# print(oddities([]) == [])                      # True
+
+# Data Structures
+#   - lists
+
+# Algorithm
+# - Declare a function oddities that takes a single argument of type list
+#   - Initialize an empty list result
+#   - Iterate over the odd elements of the original list
+#       - Append the elements to the result list
+#   - Return result
+
+# Code
+def oddities(lst):
+    result = []
+    for idx in range(0, len(lst), 2):
+        result.append(lst[idx])
+
+    return result
+
+
+def oddities2(lst):
+    return lst[::2]
+
+
+print(oddities([2, 3, 4, 5, 6]) == [2, 4, 6])  # True
+print(oddities([1, 2, 3, 4]) == [1, 3])        # True
+print(oddities(["abc", "def"]) == ['abc'])     # True
+print(oddities([123]) == [123])                # True
+print(oddities([]) == [])                      # True
+
+print(oddities2([2, 3, 4, 5, 6]) == [2, 4, 6])  # True
+print(oddities2([1, 2, 3, 4]) == [1, 3])        # True
+print(oddities2(["abc", "def"]) == ['abc'])     # True
+print(oddities2([123]) == [123])                # True
+print(oddities2([]) == [])                      # True

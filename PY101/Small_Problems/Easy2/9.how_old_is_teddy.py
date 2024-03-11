f915938f0a8baadb4b5becf979dd15f5bd8d68e7
Iván E. Hernández T <iamieht@gmail.com>
2024-03-11 15:19:26 +0100

    Create 9.how_old_is_teddy.py
    Easy2 - How old is Teddy

---
+++ PY101/Small_Problems/Easy2/9.how_old_is_teddy.py
@@ -0,0 +1,32 @@
+# Problem (P)
+# Build a program that randomly generates and prints Teddy's age. To get the age, you should generate a random number between 20 and 100, inclusive.
+
+# - Input:
+#   - Nothing
+# - Output:
+#   - a random integer between 20 and 100 (inclusive)
+
+# - Explicit Rules:
+
+# - Implicit Rules:
+
+# Examples/Test Cases (E)
+# Teddy is 69 years old!
+
+# Data Structures
+
+# Algorithm
+# - Declare a function main
+# - Initialize a variable years = random.rand(20, 100)
+# - Print(f"Teddy is {years} old!")
+
+# Code
+import random
+
+
+def main():
+    years = random.randint(20, 100)
+    print(f"Teddy is {years} years old!")
+
+
+main()
